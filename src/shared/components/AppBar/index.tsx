b73f1de5667cbe012405a03c6ed5dbcec95ca432
Geovane <geovanebpjr@gmail.com>
2023-06-05 05:13:36 -0300

    feat: correcting imports

--- src/shared/components/AppBar/index.tsx
+++ src/shared/components/AppBar/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { styled, alpha } from '@mui/material/styles';
-import { AppBar as MUIAppBar } from '@mui/material/AppBar';
+import { AppBar as MUIAppBar } from '@mui/material';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
